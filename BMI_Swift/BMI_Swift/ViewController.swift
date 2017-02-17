91dea7501e58c4ee1a50f63a4d4b5b367d3059ad
李旻 <limin@liminde-MacBook-Pro.local>
2017-02-17 11:23:20 +0800

    remember check .git

---
+++ BMI_Swift/BMI_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BMI_Swift
+//
+//  Created by 李旻 on 2017/2/16.
+//  Copyright © 2017年 里民. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BMI_Swift/BMI_SwiftTests/BMI_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  BMI_SwiftTests.swift
+//  BMI_SwiftTests
+//
+//  Created by 李旻 on 2017/2/16.
+//  Copyright © 2017年 里民. All rights reserved.
+//
+
+import XCTest
+@testable import BMI_Swift
+
+class BMI_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
